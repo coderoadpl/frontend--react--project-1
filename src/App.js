b1f35bea1c528c3b83842e640ba3cadd148b6e05
Mateusz Choma <kontakt@coderoad.pl>
2021-07-14 18:46:07 +0200

    Initial state added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react'
 
-export const App = () => {
-  return (
-    <div>
-      CodeRoad APP
-    </div>
-  )
+export class App extends React.Component {
+  state = {
+    randomNumber: null
+  }
+
+  render () {
+    const { randomNumber } = this.state
+
+    return (
+      <div>
+        CodeRoad APP
+        <br />
+        <br />
+        {
+          randomNumber === null ?
+            'Waiting for first number'
+            :
+            randomNumber
+        }
+      </div>
+    )
+  }
 }
 
 export default App
